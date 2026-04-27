72033aec9512e97804dbaa708a89478eae4ab49f
valentina <valentina.orozco@nylim.com>
2026-04-27 16:09:52 -0400

    first commit

---
+++ Test_File.py
@@ -0,0 +1,15 @@
+fh = open('FF_tiny.txt')
+
+lines = fh.readlines()
+
+for line in lines:
+    firstcol = line.split()[0]
+    seccol = line.split()[1]
+    thirdcol = line.split()[2]
+    fourcol = line.split()[3]
+    lastcol = line.split()[-1]
+    year = firstcol[:4]
+    yearformatted = int(year)
+    if yearformatted > 1926:
+        print(yearformatted,seccol, thirdcol,fourcol, lastcol)
+
